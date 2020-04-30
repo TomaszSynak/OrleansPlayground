501011e5267bc74daad47a575ece0cc9047d46a9
Tomasz Synak <tomasz.synak@outlook.com>
2020-04-30 14:25:06 +0200

    [patch] (Infrastructure) move StorageConnectionString to appsettings.json

--- Projects/API/Program.cs
+++ Projects/API/Program.cs
@@ -2,7 +2,6 @@ namespace ASPNetCoreHostedServices
 {
     using System.Net;
     using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
@@ -13,38 +12,25 @@ namespace ASPNetCoreHostedServices
 
     public class Program
     {
-        private const string ConnectionString = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+        private static string _connectionString = string.Empty;
 
         public static Task Main(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.Configure((ctx, app) =>
-                    {
-                        if (ctx.HostingEnvironment.IsDevelopment())
-                        {
-                            app.UseDeveloperExceptionPage();
-                        }
-
-                        app.UseHttpsRedirection();
-                        app.UseRouting();
-                        app.UseAuthorization();
-                        app.UseEndpoints(endpoints =>
-                        {
-                            endpoints.MapControllers();
-                        });
-                    });
+                    webBuilder.Configure(Startup.Configure);
                 })
-                .ConfigureServices(services =>
+                .ConfigureServices((hostBuilderContext, services) =>
                 {
                     services.AddControllers();
+                    _connectionString = hostBuilderContext.Configuration["StorageConnectionString"];
                 })
                 .UseOrleans(siloBuilder =>
                 {
                     siloBuilder
                         .UseAzureStorageClustering(options =>
                         {
-                            options.ConnectionString = ConnectionString;
+                            options.ConnectionString = _connectionString;
                         })
                         .ConfigureEndpoints(siloPort: 11111, gatewayPort: 30000)
                         .ConfigureLogging(options => options.SetMinimumLevel(LogLevel.Warning))
@@ -69,7 +55,7 @@ namespace ASPNetCoreHostedServices
                          {
                              opts.UseJson = true;
                              opts.ContainerName = "orleans-container";
-                             opts.ConnectionString = ConnectionString;
+                             opts.ConnectionString = _connectionString;
                          })
                         .UseDashboard(opts =>
                         {
---
+++ Projects/API/Startup.cs
@@ -0,0 +1,26 @@
+﻿namespace ASPNetCoreHostedServices
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public static void Configure(WebHostBuilderContext ctx, IApplicationBuilder app)
+        {
+            if (ctx.HostingEnvironment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
