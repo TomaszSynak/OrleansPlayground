20a84e38c0bd19935ccbcbe1f490073c588ff813
Tomasz Synak <tomasz.synak@outlook.com>
2020-05-28 01:58:11 +0200

    [minor] (HotDesk) define HotDesk interface and write its implementation

---
+++ Projects/Interfaces/HotDesk/IHotDesk.cs
@@ -0,0 +1,14 @@
+﻿namespace Interfaces.HotDesk
+{
+    using System;
+    using System.Collections.Immutable;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface IHotDesk : Orleans.IGrainWithStringKey
+    {
+        Task<IImmutableList<Reservation>> GetReservations(CancellationToken cancellationToken = default);
+
+        Task<bool> TryReserveHotDesk(DateTime reservationDate, string developerEmail, CancellationToken cancellationToken = default);
+    }
+}
