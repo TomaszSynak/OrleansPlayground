e77ba2e164a9495132ce51d26bd1f9a975406817
Tomasz Synak <tomasz.synak@outlook.com>
2020-05-28 01:58:43 +0200

    [minor] (HotDesk) expose HotDesk through API

---
+++ Projects/API/Components/HotDesk/HotDeskController.cs
@@ -0,0 +1,44 @@
+﻿namespace HotDesk.Components.HotDesk
+{
+    using System.Net;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Interfaces;
+    using Interfaces.HotDesk;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Orleans;
+
+    [ApiController]
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class HotDeskController : ControllerBase
+    {
+        private readonly IGrainFactory _client;
+
+        public HotDeskController(IGrainFactory client)
+        {
+            _client = client;
+        }
+
+        [HttpGet("{hotDeskId}")]
+        public async Task<IActionResult> GetHotDeskReservation(string hotDeskId, CancellationToken cancellationToken)
+        {
+            var hotDesk = _client.GetGrain<IHotDesk>(hotDeskId);
+            return StatusCode((int)HttpStatusCode.OK, await hotDesk.GetReservations(cancellationToken));
+        }
+
+        [HttpPost("{hotDeskId}/reserve")]
+        public async Task<IActionResult> ReserveHotDesk(string hotDeskId, HotDeskReservationDto reservationDto, CancellationToken cancellationToken)
+        {
+            var hotDesk = _client.GetGrain<IHotDesk>(hotDeskId);
+
+            var reserved = await hotDesk.TryReserveHotDesk(reservationDto.ReservationDate, reservationDto.DeveloperEmail, cancellationToken);
+
+            return reserved
+                ? StatusCode((int)HttpStatusCode.Created)
+                : StatusCode((int)HttpStatusCode.Conflict);
+        }
+    }
+}
---
+++ Projects/API/Components/HotDesk/HotDeskReservationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HotDesk.Components.HotDesk
+{
+    using System;
+
+    public class HotDeskReservationDto
+    {
+        public string DeveloperEmail { get; set; }
+
+        public DateTime ReservationDate { get; set; }
+    }
+}
